47a6577e110b407d832f41f59e905a2a978ceda0
n4vneetSin9h <dev.nav94@gmail.com>
2025-10-22 22:39:57 +0530

    chapter 2 q1

--- Exercises/src/main/java/org/example/ChapterTwo/ChapterTwoAnswers.java
+++ Exercises/src/main/java/org/example/ChapterTwo/ChapterTwoAnswers.java
@@ -1,7 +1,18 @@
 package org.example.ChapterTwo;
 
+import java.util.Scanner;
+
 public class ChapterTwoAnswers {
     public static void main(String[] args) {
+        answerOne();
+    }
 
+    public static void answerOne() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter miles: ");
+        double miles = scanner.nextDouble();
+
+        System.out.printf("%.2f miles is %.2f kilometers", miles,  miles * 1.6);
     }
 }
